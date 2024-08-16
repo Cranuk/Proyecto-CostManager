e56296124f387d70d049fc56b8c6c7988a0819db
Cranuk <facundom4t0@gmail.com>
2024-08-16 16:14:15 -0300

    se agrego favicon

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -9,6 +9,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;900&display=swap">
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <link rel="icon" href="{{ asset('Proyectos.png')}}">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>CostManager - @yield('title')</title>
         @vite([
